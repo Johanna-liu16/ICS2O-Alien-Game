d53f2ff9ee7054b383846bc0f040b6db175bdd9d
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-04-26 12:01:54 +0000

    added code

--- js/splashScene.js
+++ js/splashScene.js
@@ -14,9 +14,9 @@ class SplashScene extends Phaser.Scene {
    * This method is the construtor.
    */
   constructor() {
-    super({ key: "splashScene" });
+    super({ key: "splashScene" })
   }
-
+    
   /**
    * Can be defined on your own Scenes.
    * This method is called by the Scene Manager when the scene starts,
@@ -24,15 +24,15 @@ class SplashScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   init(data) {
-    this.cameras.main.setBackgroundColor("ffffff");
+    this.cameras.main.setBackgroundColor("ffffff")
   }
-
+    
   /**
    * Can be defined on your own Scenes.
    * Use it to load assets.
    */
   preload() {
-    console.log("Splash Scene");
+    console.log("Splash Scene")
   }
 
   /**
@@ -41,9 +41,9 @@ class SplashScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create(data) {
-    // pass
+      //pass 
   }
-
+    
   /**
    * Should be overridden by your own Scenes.
    * This method is called once per game step while the scene is running.
@@ -51,8 +51,8 @@ class SplashScene extends Phaser.Scene {
    *  @param {number} delta - The delta time in ms since the last frame.
    */
   update(time, delta) {
-    this.scene.switch("titlescene");
+    this.scene.switch("titleScene")
   }
 }
 
-export default SplashScene;
+export default SplashScene
