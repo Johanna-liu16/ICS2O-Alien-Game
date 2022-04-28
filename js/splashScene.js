6d7c4a79a00bfc392118e31ebfc9e6caab913397
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-04-28 00:12:24 +0000

    add

--- js/splashScene.js
+++ js/splashScene.js
@@ -15,8 +15,10 @@ class SplashScene extends Phaser.Scene {
    */
   constructor() {
     super({ key: "splashScene" })
+
+    this.splashSceneBackgroundImage = null
   }
-    
+
   /**
    * Can be defined on your own Scenes.
    * This method is called by the Scene Manager when the scene starts,
@@ -24,15 +26,16 @@ class SplashScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   init(data) {
-    this.cameras.main.setBackgroundColor("ffffff")
+    this.cameras.main.setBackgroundColor("#ffffff")
   }
-    
+
   /**
    * Can be defined on your own Scenes.
    * Use it to load assets.
    */
   preload() {
     console.log("Splash Scene")
+    this.load.image("splashSceneBackground", "./assets/splashSceneImage.png")
   }
 
   /**
@@ -41,9 +44,15 @@ class SplashScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create(data) {
-      //pass 
+    this.splashSceneBackgroundImage = this.add.sprite(
+      0,
+      0,
+      "splashSceneBackground"
+    )
+    this.splashSceneBackgroundImage.x = 1920 / 2
+    this.splashSceneBackgroundImage.y = 1080 / 2
   }
-    
+
   /**
    * Should be overridden by your own Scenes.
    * This method is called once per game step while the scene is running.
@@ -51,7 +60,9 @@ class SplashScene extends Phaser.Scene {
    *  @param {number} delta - The delta time in ms since the last frame.
    */
   update(time, delta) {
-    this.scene.switch("titleScene")
+    if (time > 3000) {
+      this.scene.switch("titleScene")
+    }
   }
 }
 
