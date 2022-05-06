8d4624800f326d1dc54c3f3cabd1a91a9737834f
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-05-05 20:12:51 -0400

    add

--- js/gameScene.js
+++ js/gameScene.js
@@ -6,53 +6,36 @@
 // Created on: April 2022
 // This is the Game Scene
 
-/**
- * This class is the Game Scene.
- */
 class GameScene extends Phaser.Scene {
-  /**
-   * This method is the constructor
-   */
   constructor() {
-    super({ key: "gameScene" });
+    super({ key: "gameScene" })
+
+    this.background = null
+    this.ship = null
   }
 
-  /**
-   * Can be defined on your own Scenes.
-   * this method is called by the Scene Manager when the scene starts,
-   * before preload() and create().
-   * @param {object} data - Any data via ScenePlugin.add() or ScenePlugin.start().
-   */
   init(data) {
-    this.cameras.main.setBackgroundColor("#ffffff");
+    this.cameras.main.setBackgroundColor("#ffffff")
   }
 
-  /**
-   * Can be defined on your own Scenes.
-   * Use it to load assets.
-   */
   preload() {
-    console.log("Game Scene");
+    console.log("Game Scene")
+
+    //images
+    this.load.image("starBackground", "assets/starBackground.png")
+    this.load.image("ship", "assets/spaceShip.png")
   }
 
-  /**
-   * Can be defined on your own Scenes.
-   * Use it to create your game objects.
-   * @param {object} data - Any data via ScenePlugin.add() or ScenePlugin.start().
-   */
   create(data) {
+    this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
+    this.background.setOrigin(0, 0)
+
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
     //pass
   }
 
-  /**
-   * Should be overridden by your own Scenes.
-   * This method is called once per game step while the scene is running.
-   * @param {number} time - the current time.
-   * @param {number} delta - The delta time in ms since the last frame.
-   */
   update(time, delta) {
-    // pass
   }
 }
 
-export default GameScene;
+export default GameScene
