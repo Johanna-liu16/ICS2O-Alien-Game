ac1a2e724f87f47f0acb22a16a257e2385a876ec
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-05-23 15:16:26 +0000

    fix

--- js/gameScene.js
+++ js/gameScene.js
@@ -33,10 +33,10 @@ class GameScene extends Phaser.Scene {
     this.fireMissile = false;
     this.score = 0;
     this.scoreText = null;
-    this.scoreTextStyle = { 
-        font: '65px Arial', 
-        fill: '#ffffff', 
-        align: 'center'}
+    this.scoreTextStyle = {
+       font: '65px Arial', 
+       fill: '#ffffff', 
+       align: 'center'}
   }
 
   /**
@@ -75,10 +75,10 @@ class GameScene extends Phaser.Scene {
     this.background.setOrigin(0, 0);
 
     this.scoreText = this.add.text(
-        10, 
-        10, 
-        "Score: " + this.score.toString(), 
-        this.scoreTextStyle
+      10, 
+      10, 
+     "Score: " + this.score.toString(), 
+      this.scoreTextStyle
     );
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship");
@@ -99,7 +99,7 @@ class GameScene extends Phaser.Scene {
         missileCollide.destroy();
         this.sound.play("explosion");
         this.score = this.score + 1;
-        this.scoreText.setText ("Score: " + this.score.toString());
+        this.scoreText.setText("Score: " + this.score.toString());
         this.createAlien();
         this.createAlien();
       }.bind(this)
