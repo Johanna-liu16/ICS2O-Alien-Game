ffffcf02d8cbf49beadbf220a4689e836bd5c4df
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-05-23 13:43:42 +0000

    fix

--- js/gameScene.js
+++ js/gameScene.js
@@ -10,8 +10,9 @@
  * This class is the Game Scene.
  */
 class GameScene extends Phaser.Scene {
-
-  // create an alien
+  /** 
+   * create an alien
+  */
   createAlien() {
     const alienXLocation = Math.floor(Math.random() * 1920) + 1; // this will get a number between 1 and 1920
     let alienXVelocity = Math.floor(Math.random() * 50) + 1; // this will get a number between 1 and 50
@@ -79,8 +80,8 @@ class GameScene extends Phaser.Scene {
 
     // create a group for the aliens
     this.physics.add.collider(
-        this.missileGroup, 
-        this.alienGroup, 
+this.missileGroup, 
+this.alienGroup, 
         function (missileCollide, alienCollide) {
         alienCollide.destroy();
         missileCollide.destroy();
