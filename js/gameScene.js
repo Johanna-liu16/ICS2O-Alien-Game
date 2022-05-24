20de5289db9b209222fcdb3986886175aaabfbd5
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-05-24 14:52:13 +0000

    add

--- js/gameScene.js
+++ js/gameScene.js
@@ -121,16 +121,17 @@ class GameScene extends Phaser.Scene {
         this.physics.pause();
         alienCollide.destroy();
         shipCollide.destroy();
-        this.gameOverText =
-            this.add.text(
+        this.gameOverText = this.add
+            .text(
             1920 / 2,
             1080 / 2,
             "Game Over!\nClick to play again.",
             this.gameOverTextStyle
-          ).setOrigin(0.5);
+          ).
+            setOrigin(0.5);
         this.gameOverText.setInteractive({ useHandCursor: true });
         this.gameOverText.on("pointerdown", () =>
-          this.scene.start("gameScene")
+             this.scene.start("gameScene")
      );
       }.bind(this)
     );
