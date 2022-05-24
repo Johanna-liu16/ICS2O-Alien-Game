070b965b31c67c3838bd0b6aa847ee5f28f4b365
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-05-24 13:03:22 +0000

    added

--- js/gameScene.js
+++ js/gameScene.js
@@ -38,6 +38,7 @@ class GameScene extends Phaser.Scene {
       fill: "#ffffff",
       align: "center",
     };
+    this.gameOverTextStyle = { font: "65px Arial", fill: "#ff0000", align: "center" }
   }
 
   /**
@@ -47,7 +48,7 @@ class GameScene extends Phaser.Scene {
    * @param {object} data - Any data via ScenePlugin.add() or ScenePlugin.start().
    */
   init(data) {
-    this.cameras.main.setBackgroundColor("#ffffff");
+    this.cameras.main.setBackgroundColor("#0x5f6e7a");
   }
 
   /**
@@ -64,6 +65,7 @@ class GameScene extends Phaser.Scene {
     // sound
     this.load.audio("laser", "assets/laser1.wav");
     this.load.audio("explosion", "assets/barrelExploding.wav");
+    this.load.audio("bomb", "assets/bomb.wav")
   }
 
   /**
@@ -91,7 +93,7 @@ class GameScene extends Phaser.Scene {
     this.alienGroup = this.add.group();
     this.createAlien();
 
-    // create a group for the aliens
+    // Collisions between missiles and aliens
     this.physics.add.collider(
       this.missileGroup,
       this.alienGroup,
@@ -105,6 +107,22 @@ class GameScene extends Phaser.Scene {
         this.createAlien();
       }.bind(this)
     );
+
+    // Collisions between ship and aliens
+    this.physics.add.collider(
+      this.ship,
+      this.alienGroup,
+      function (shipCollide, alienCollide) {
+        this.sound.play("bomb");
+        this.physics.pause()
+        alienCollide.destroy();
+        shipCollide.destroy();
+        this.gameOverText = this.add.text(1920 / 2, 1080 / 2, "Game Over!\nClick to play again.", this.gameOverTextStyle).setOrigin(0.5);
+        this.gameOverText.setInteractive({ useHandCursor: true});
+        this.gameOverText.on("pointerdown", () => this.scene.start("gameScene"));
+      }.bind(this)
+    );
+  
   }
 
   /**
