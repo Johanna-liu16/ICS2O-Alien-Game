a1c062663041d07e6164c8412ce48a0141ab543b
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-05-24 14:17:01 +0000

    fix

--- js/gameScene.js
+++ js/gameScene.js
@@ -38,7 +38,11 @@ class GameScene extends Phaser.Scene {
       fill: "#ffffff",
       align: "center",
     };
-    this.gameOverTextStyle = { font: "65px Arial", fill: "#ff0000", align: "center" }
+    this.gameOverTextStyle = { 
+        font: "65px Arial", 
+        fill: "#ff0000", 
+        align: "center",
+    };
   }
 
   /**
@@ -65,7 +69,7 @@ class GameScene extends Phaser.Scene {
     // sound
     this.load.audio("laser", "assets/laser1.wav");
     this.load.audio("explosion", "assets/barrelExploding.wav");
-    this.load.audio("bomb", "assets/bomb.wav")
+    this.load.audio("bomb", "assets/bomb.wav");
   }
 
   /**
@@ -114,10 +118,15 @@ class GameScene extends Phaser.Scene {
       this.alienGroup,
       function (shipCollide, alienCollide) {
         this.sound.play("bomb");
-        this.physics.pause()
+        this.physics.pause();
         alienCollide.destroy();
         shipCollide.destroy();
-        this.gameOverText = this.add.text(1920 / 2, 1080 / 2, "Game Over!\nClick to play again.", this.gameOverTextStyle).setOrigin(0.5);
+        this.gameOverText = this.add.text(
+            1920 / 2,
+            1080 / 2,
+            "Game Over!\nClick to play again.",
+            this.gameOverTextStyle
+        ).setOrigin(0.5);
         this.gameOverText.setInteractive({ useHandCursor: true});
         this.gameOverText.on("pointerdown", () => this.scene.start("gameScene"));
       }.bind(this)
